0f6e4a470d69c2e642feedfbda512dda025e4df1
Fuddles <perso@Elior-MacBook-Air.local>
2016-12-28 17:25:44 -0800

    12123312341234542123456788543212367890

--- lightup.js
+++ lightup.js
@@ -1,39 +1,45 @@
-// Light up the LED strip. 72 LEDs
+try {
 
-console.log("Begin lightup");
+  // Light up the LED strip. 72 LEDs
 
-//var debug = require('debug')('lightup');
+  console.log("Begin lightup");
 
-
-var rpio = require('rpio');
-
-// We begin!
-
-console.log("Begin rpio SPI");
-
-rpio.spiBegin();
-
-// TODO
-//rpio.spiSetClockDivider(4); 	/* divider should be 4 or 8 max to have high-speed display */
-rpio.spiSetClockDivider(128); 	/* divider should be 4 or 8 max to have high-speed display */
-
-var txbuf = new Buffer([0x0, 0x0, 0x0, 0x0,
-
-  // Couleur de la 1e LED
-  0xff, 0x0, 0x0, 0xff,
-  // Couleur de la 2e LED
-  0xff, 0x0, 0xff, 0x0,
-  // Couleur de la 3e LED
-  0xff, 0xff, 0x0, 0x0,
-  // Couleur de la 4e LED
-  0xff, 0xff, 0xff, 0xff,
+  //var debug = require('debug')('lightup');
 
 
-   0xff, 0xff, 0xff, 0xff, 0xff
-]);
+  var rpio = require('rpio');
 
-rpio.spiWrite(txbuf, txbuf.length);
+  // We begin!
 
-rpio.spiEnd();
+  console.log("Begin rpio SPI");
 
-console.log("End rpio SPI");
+  rpio.spiBegin();
+
+  // TODO
+  //rpio.spiSetClockDivider(4); 	/* divider should be 4 or 8 max to have high-speed display */
+  rpio.spiSetClockDivider(128); 	/* divider should be 4 or 8 max to have high-speed display */
+
+  var txbuf = new Buffer([0x0, 0x0, 0x0, 0x0,
+
+    // Couleur de la 1e LED
+    0xff, 0x0, 0x0, 0xff,
+    // Couleur de la 2e LED
+    0xff, 0x0, 0xff, 0x0,
+    // Couleur de la 3e LED
+    0xff, 0xff, 0x0, 0x0,
+    // Couleur de la 4e LED
+    0xff, 0xff, 0xff, 0xff,
+
+
+     0xff, 0xff, 0xff, 0xff, 0xff
+  ]);
+
+  rpio.spiWrite(txbuf, txbuf.length);
+
+  rpio.spiEnd();
+
+  console.log("End rpio SPI");
+} catch(e) {
+  console.log("[begin lightup] exception:" + e.name + " -- " + e.message);
+  console.log(e);
+}
