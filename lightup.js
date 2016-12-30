9726bad1ab2fedf88c1edd5963c7173120966f0b
Fuddles <perso@Elior-MacBook-Air.local>
2016-12-29 18:18:58 -0800

    tt

--- lightup.js
+++ lightup.js
@@ -37,14 +37,14 @@ try {
         }
 
         i = 0;
-        txbuf.writeUInt8( 255,    startPadBytes +     i * 4);
+        txbuf.writeUInt8( 0,      startPadBytes +     i * 4);
         txbuf.writeUInt8( 255,      startPadBytes + 1 + i * 4);
-        txbuf.writeUInt8( 255,      startPadBytes + 2 + i * 4);
+        txbuf.writeUInt8( 0,      startPadBytes + 2 + i * 4);
         txbuf.writeUInt8( 255,      startPadBytes + 3 + i * 4);
 
         i = 1;
-        txbuf.writeUInt8( 255,    startPadBytes +     i * 4);
-        txbuf.writeUInt8( 255,    startPadBytes + 1 + i * 4);
+        txbuf.writeUInt8( 0,    startPadBytes +     i * 4);
+        txbuf.writeUInt8( 0,    startPadBytes + 1 + i * 4);
         txbuf.writeUInt8( 255,    startPadBytes + 2 + i * 4);
         txbuf.writeUInt8( 255,    startPadBytes + 3 + i * 4);
 
