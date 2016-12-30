5fa6da1b477bdc969664680759e31f4862fa201e
Fuddles <perso@Elior-MacBook-Air.local>
2016-12-29 16:22:02 -0800

    Marche!

--- lightup.js
+++ lightup.js
@@ -22,7 +22,7 @@ try {
     rpio.spiSetClockDivider(64); 	// divider should be 4 or 8 max to have high-speed display
 
     var startPadBytes = 4;
-    var endPadBytes   = 12;
+    var endPadBytes   = 32;
     var brightness = 7;
     var txbuf    = Buffer.allocUnsafe( startPadBytes + 72*4 + endPadBytes );
     var i, loop = 0;
@@ -58,21 +58,21 @@ try {
             txbuf.writeUInt8( 0x0,                  startPadBytes + 195 + i * 4);
         }
 
-
-    /*
+/*
         // 72 LEDs  (<0xE0+brightness> <blue> <green> <red>) brightness 0..31
-        for (i = 4; i < 292; i += 4) {              // 4 * 72 + 4
-            txbuf.writeUInt8( 0xef, i );            // Half brightness
-            txbuf.writeUInt8( Math.floor( 256 * Math.random() ), i + 1 );
-            txbuf.writeUInt8( Math.floor( 256 * Math.random() ), i + 2 );
-            txbuf.writeUInt8( Math.floor( 256 * Math.random() ), i + 3 );
+        for (i = 0; i < 4*72; i += 4) {              // 4 * 72 + 4
+            txbuf.writeUInt8( 0xef,                              i + startPadBytes );            // Half brightness
+            txbuf.writeUInt8( Math.floor( 256 * Math.random() ), i + startPadBytes + 1 );
+            txbuf.writeUInt8( Math.floor( 256 * Math.random() ), i + startPadBytes + 2 );
+            txbuf.writeUInt8( Math.floor( 256 * Math.random() ), i + startPadBytes + 3 );
         }
-    */
+*/
+
         // End frame: at least (n/2) bits of 1, where n = 72 LEDs. Here we send 8 bytes
         // 8 x bytes filled with 0 to init
 
         for (i = 0; i < endPadBytes; i++) {
-            txbuf.writeUInt8( 0x00, startPadBytes + 292 + i );
+            txbuf.writeUInt8( 0x00, startPadBytes + 4*72 + i );
         }
 
         // Send to the LED strip
