efe908025adf0c8f23ac2fb9319883d4b860b3f1
Fuddles <perso@Elior-MacBook-Air.local>
2017-01-01 20:02:04 -0800

    multer upload

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+// Lea, Jan 2017
+
 var express = require('express');
 var router = express.Router();
 
@@ -7,12 +9,39 @@ router.get('/', function(req, res, next) {
 });
 
 
-// Upload of a new photo
-router.post('/upload-photo', function(req, res, next) {
 
-    // TODO
+// ---------------- Upload of a new photo --------------------------------------
 
-    console.log(" API /upload-photo called ");
+const multer  = require('multer');
+
+const UPLOAD_DIR        = require(../process/image-functions).UPLOAD_DIR;
+const cropResizePromise = require(../process/image-functions).cropResizePromise;
+
+var uploadWithMulter = multer({
+    dest:       UPLOAD_DIR,
+    fileFilter: function(req, file, cb) {
+            // Check that it is an image:
+            if ( req.file.mimeType == 'image/png' || req.file.mimeType == 'image/gif' || req.file.mimeType == 'image/jpeg' ) {
+                // To accept the file pass `true`, like so:
+                cb(null, true);
+                return;
+            }
+            console.error("ERROR in multer upload: WRONG MIME TYPE (not an image) = " + req.file.mimeType );
+            // To reject this file pass `false`, like so:
+            return cb(null, false);
+        },
+    limits: {
+            fileSize: 100000000          // in bytes. Image size max is 100MB
+        }
+});
+
+// See multer doc at https://www.npmjs.com/package/multer
+router.post('/upload-photo', uploadWithMulter.single('myImage'), function(req, res, next) {
+
+    console.log(" API /upload-photo called, file [" + req.file.filename + "] saved! ");
+
+    // TODO: call cropAndResize + display on LED
+
     return;
 });
 
