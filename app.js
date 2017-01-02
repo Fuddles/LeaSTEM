02edeaf6bc64e32d8bf02f8025550e10068f31bc
Fuddles <perso@Elior-MacBook-Air.local>
2017-01-01 21:39:17 -0800

    upload.js

--- app.js
+++ app.js
@@ -25,6 +25,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+
+// --- Photo upload
+var uploadWithMulter = require('./routes/upload').uploadWithMulter;
+var uploadPhotoPost  = require('./routes/upload').uploadPhotoPost;
+app.post( '/upload-photo', uploadWithMulter.single('myphoto'), uploadPhotoPost );
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
---
+++ routes/upload.js
@@ -0,0 +1,49 @@
+// Lea, Jan 2017
+
+const multer = require('multer');
+
+const UPLOAD_DIR     = require("../process/image-functions").UPLOAD_DIR;
+
+var uploadWithMulter = multer({
+    dest:       UPLOAD_DIR
+});
+/*
+var uploadWithMulter = multer({
+    dest:       UPLOAD_DIR,
+    fileFilter: function(req, file, cb) {
+            console.log( "In fileFilter, with file.originalname = "+ file.originalname +", and file.mimeType = " + file.mimeType );
+            // Check that it is an image:
+            if ( file.mimeType == 'image/png' || file.mimeType == 'image/gif' || file.mimeType == 'image/jpeg' ) {
+                // To accept the file pass `true`, like so:
+                cb(null, true);
+                return;
+            }
+            console.error("ERROR in multer upload: WRONG MIME TYPE (not an image) = " + req.file.mimeType );
+            // To reject this file pass `false`, like so:
+            return cb(null, false);
+        },
+    limits: {
+            fileSize: 100000000          // in bytes. Image size max is 100MB
+        }
+});
+*/
+
+
+// ---------------- Upload of a new photo --------------------------------------
+
+const cropResizePromise = require("../process/image-functions").cropResizePromise;
+
+// See multer doc at https://www.npmjs.com/package/multer
+function uploadPhotoPost(req, res, next) {
+
+    console.log(" API /upload-photo called, file saved?? ");
+    //console.log(" API /upload-photo called, file [" + req.file.filename + "] saved! ");
+
+    // TODO: call cropAndResize + display on LED
+
+    return res.sendStatus(200);
+});
+
+
+module.exports.uploadWithMulter = uploadWithMulter;
+module.exports.uploadPhotoPost  = uploadPhotoPost;
