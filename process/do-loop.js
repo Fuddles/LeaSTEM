49c1a5e61aaf4aa3d4ef6c399403aa210ace6f38
Fuddles <perso@Elior-MacBook-Air.local>
2017-02-19 18:02:31 -0800

    bf

--- process/do-loop.js
+++ process/do-loop.js
@@ -84,8 +84,8 @@ function doLedDisplayLoop() {
     let currentAngle    = angle;
     let angleDiff       = hrTimeDiff[1] * 1.0e-9 * angularVelocity;
     if ( Math.abs(angleDiff) >= 0.1 ) {
-        currentAngle    = (angle + angleDiff + 360 ) % 360;
-        if ( Math.abs(angleDiff) >= 10 ) {            
+        currentAngle    = (angle - angleDiff + 360 ) % 360;
+        if ( Math.abs(angleDiff) >= 10 ) {
             console.log( "DIFF angle with velocity:  angle="+ angle +", angleDiff="+ angleDiff
             + ", \t angularVelocity="+ angularVelocity +" deg/s, hrTimeDiff="+ hrTimeDiff );
         }
