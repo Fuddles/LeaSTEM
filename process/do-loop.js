7e6ff4b3a66c471c0272147530c3a635f065618b
Fuddles <perso@Elior-MacBook-Air.local>
2017-02-22 11:46:07 -0800

    bf

--- process/do-loop.js
+++ process/do-loop.js
@@ -177,7 +177,7 @@ function _keepPreviousDataPointsAndFindMaxMagZToComputeAngleCorrectionFromBottom
     //   -- [2][] is a local max
 
     // At small angular speed, we want close to the max, but at higher speed the value is lower
-    let maxCompMagZ = ( angularVelocity < 90 ? 0.5 : 0.1 ) * magZMaxValue;
+    let maxCompMagZ = ( angularVelocity < 90 ? 0.7 : 0.2 ) * magZMaxValue;
     if ( previousDataPoints[2][2] > maxCompMagZ && Math.abs( angularVelocity ) > 1.0 ) {
         // Test constant sign of angularVelocity
         if (  (   previousDataPoints[0][3] > 0 && previousDataPoints[1][3] > 0 && previousDataPoints[2][3] > 0
