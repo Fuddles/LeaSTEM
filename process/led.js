a2fd2d064bc581ceb5f0e7c7ec7b78cb8530f6dd
Fuddles <perso@Elior-MacBook-Air.local>
2017-02-25 18:49:52 -0800

    test

--- process/led.js
+++ process/led.js
@@ -22,7 +22,7 @@ rpio.init({
 rpio.spiBegin();
 //rpio.spiSetClockDivider(4); 	// divider should be 4 or 8 max to have high-speed display, but flickers --> 64 is good
 //rpio.spiSetClockDivider(64);
-rpio.spiSetClockDivider(32);
+rpio.spiSetClockDivider(512);
 rpio.spiSetDataMode(0);
 
 
